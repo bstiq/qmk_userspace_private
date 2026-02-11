4302d6ae40622911d5fc93bbf59e571a39fed8bc
Regulus-Robo-Design <rodrinom211@gmail.com>
2026-02-11 21:40:15 +0100

    Jero
    Return

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/LOGO/logo.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-// Función para dibujar la imagen en un framebuffer
-void logo_draw(uint16_t *fb, int width, int height);
-
--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/LOGO/logo_data.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-extern const int logo_w;
-extern const int logo_h;
-extern const uint16_t logo_img[];
-

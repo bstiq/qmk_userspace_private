db54cbab58533e6d675d6beb1847aae0b26f3251
Regulus-Robo-Design <rodrinom211@gmail.com>
2026-02-14 20:15:45 +0100

    ggg
    fgd

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/rules.mk
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/rules.mk
@@ -11,6 +11,10 @@ SRC += gfx/bar_blue.qgf.c
 SRC += gfx/bar_green.qgf.c
 SRC += gfx/bar_gray.qgf.c
 SRC += gfx/fonts.qff.c
+SRC += logo.c
+SRC += logo_data.c
+
+
 
 BACKLIGHT_ENABLE = yes
 BACKLIGHT_DRIVER = pwm

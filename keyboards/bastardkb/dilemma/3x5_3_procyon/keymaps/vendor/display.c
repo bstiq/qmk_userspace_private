80a8d0a27aa20835ff30b6695e5ed23ea4229bec
Quentin <bstkbd@gmail.com>
2026-02-24 14:30:33 +0100

    switch to cyan

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -177,11 +177,11 @@ void style_init_mod_indicator(void) {
     lv_style_set_radius(&style_btn, 3);
     lv_style_set_bg_opa(&style_btn, LV_OPA_COVER);
 
-    lv_style_set_bg_color(&style_btn, lv_palette_darken(LV_PALETTE_PINK, 1));
-    lv_style_set_bg_grad_color(&style_btn, lv_palette_darken(LV_PALETTE_PINK, 3));
+    lv_style_set_bg_color(&style_btn, lv_palette_darken(LV_PALETTE_CYAN, 1));
+    lv_style_set_bg_grad_color(&style_btn, lv_palette_darken(LV_PALETTE_CYAN, 3));
     lv_style_set_bg_grad_dir(&style_btn, LV_GRAD_DIR_VER);
 
-    lv_style_set_border_color(&style_btn, lv_palette_lighten(LV_PALETTE_PINK, 1));
+    lv_style_set_border_color(&style_btn, lv_palette_lighten(LV_PALETTE_CYAN, 1));
     lv_style_set_border_opa(&style_btn, LV_OPA_20);
     lv_style_set_border_width(&style_btn, 2);
 
@@ -193,11 +193,11 @@ void style_pressed_init_mod_indicator(void) {
     lv_style_set_radius(&style_btn_pressed, 2);
     lv_style_set_bg_opa(&style_btn_pressed, LV_OPA_COVER);
 
-    lv_style_set_bg_color(&style_btn_pressed, lv_palette_lighten(LV_PALETTE_PINK, 3));
-    lv_style_set_bg_grad_color(&style_btn_pressed, lv_palette_lighten(LV_PALETTE_PINK, 1));
+    lv_style_set_bg_color(&style_btn_pressed, lv_palette_lighten(LV_PALETTE_CYAN, 3));
+    lv_style_set_bg_grad_color(&style_btn_pressed, lv_palette_lighten(LV_PALETTE_CYAN, 1));
     lv_style_set_bg_grad_dir(&style_btn_pressed, LV_GRAD_DIR_VER);
 
-    lv_style_set_border_color(&style_btn_pressed, lv_palette_lighten(LV_PALETTE_PINK, 1));
+    lv_style_set_border_color(&style_btn_pressed, lv_palette_lighten(LV_PALETTE_CYAN, 1));
     lv_style_set_border_opa(&style_btn_pressed, LV_OPA_20);
     lv_style_set_border_width(&style_btn_pressed, 2);
 
@@ -207,11 +207,11 @@ void style_pressed_init_mod_indicator(void) {
 void style_bar_init(void) {
     // inner bar
     lv_style_set_radius(&style_bar, 0);
-    lv_style_set_bg_color(&style_bar, lv_palette_darken(LV_PALETTE_PINK, 3));
+    lv_style_set_bg_color(&style_bar, lv_palette_darken(LV_PALETTE_CYAN, 3));
 
     // bar background
     lv_style_set_radius(&style_bar_background, 3);
-    lv_style_set_border_color(&style_bar_background, lv_palette_darken(LV_PALETTE_PINK, 4));
+    lv_style_set_border_color(&style_bar_background, lv_palette_darken(LV_PALETTE_CYAN, 4));
     lv_style_set_border_width(&style_bar_background, 1);
 }
 

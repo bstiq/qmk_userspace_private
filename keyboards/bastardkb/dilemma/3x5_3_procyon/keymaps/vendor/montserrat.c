feb6fbaacca2512a7f57f15428b49aecba895b90
Quentin <bstkbd@gmail.com>
2026-02-26 11:12:16 +0100

    Update montserrat.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/montserrat.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/montserrat.c
@@ -242,7 +242,6 @@ lv_font_t montserrat = {
     .underline_position = -1,
     .underline_thickness = 1,
 #endif
-    .static_bitmap = 0,
     .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
     .fallback = NULL,

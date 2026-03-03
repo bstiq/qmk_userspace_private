dedb38c5017778c302ab1c07b75cb9b8cf763e9a
Quentin <bstkbd@gmail.com>
2026-03-03 15:05:11 +0100

    Update display.h

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/display.h
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/display.h
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include QMK_KEYBOARD_H
 
-typedef mod_button_pair_t Mod_button_pair_t;
+typedef struct mod_button_pair_t mod_button_pair_t;
 
 void display_init(void);
 void style_init_all(void);

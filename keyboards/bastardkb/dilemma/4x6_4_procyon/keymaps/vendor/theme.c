d603f690895dcc72059da6799695eea289a72c3d
Quentin <bstkbd@gmail.com>
2026-03-05 12:50:41 +0100

    Update theme.c

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/theme.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/theme.c
@@ -4,41 +4,41 @@ LV_FONT_DECLARE(montserratbold14);
 LV_FONT_DECLARE(montserratbold13);
 
 ui_theme default_theme = {
-    .btn_normal = {
-        .font = &montserratbold14,
-        .border_radius = 6,
-        .bg_opacity = LV_OPA_COVER,
-        .bg_color = lv_color_make(23, 26, 31),
-        .border_color = lv_color_make(50, 55, 67),
-        .text_color = lv_color_white(),
+    {
+        montserratbold14,
+        6,
+        LV_OPA_COVER,
+        lv_color_make(23, 26, 31),
+        lv_color_make(50, 55, 67),
+        lv_color_white(),
+        2,
+    },
+    {
+        montserratbold14,
+        6,
+        LV_OPA_COVER,
+        lv_color_make(71, 133, 239),
+        lv_color_white(),
+        lv_color_white(),
         .border_width = 2,
     },
-    .btn_pressed = {
-        .font = &montserratbold14,
-        .border_radius = 6,
-        .bg_opacity = LV_OPA_COVER,
-        .bg_color = lv_color_make(71, 133, 239),
-        .border_color = lv_color_white(),
-        .text_color = lv_color_white(),
-        .border_width = 2,
+    {
+        &montserratbold14,
+        6,
+        lv_color_black(),
+        lv_color_make(50, 55, 67),
+        lv_color_white(),
+        2,
     },
-    .layer_name = {
-        .font = &montserratbold14,
-        .border_radius = 6,
-        .bg_color = lv_color_black(),
-        .border_color = lv_color_make(50, 55, 67),
-        .text_color = lv_color_white(),
-        .border_width = 2,
+    {
+        &montserratbold13,
     },
-    .secondary_labels = {
-        .font = &montserratbold13,
+    {
+        0,
+        lv_color_make(71, 133, 239),
     },
-    .bar = {
-        .border_radius = 0,
-        .bg_color = lv_color_make(71, 133, 239),
-    },
-    .bar_background = {
-        .border_radius = 3,
-        .border_width = 0,
+    {
+        3,
+        0,
     },
 };

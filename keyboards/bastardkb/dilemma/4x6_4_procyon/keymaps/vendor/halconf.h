ebd95b9db9a1473b89272fb4122367e7453ebb54
Quentin <bstkbd@gmail.com>
2026-03-11 13:29:34 +0100

    move more configs

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/halconf.h
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/halconf.h
@@ -1,5 +1,3 @@
 #pragma once
 
-#define HAL_USE_PWM TRUE
-
 #include_next <halconf.h>
--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/mcuconf.h
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/mcuconf.h
@@ -1,6 +1,3 @@
 #pragma once
 
 #include_next <mcuconf.h>
-
-#undef RP_PWM_USE_PWM2
-#define RP_PWM_USE_PWM2 TRUE

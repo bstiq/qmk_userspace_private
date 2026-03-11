abcf1acb4c2c3141b35825ac52314b3c1d77133e
Quentin <bstkbd@gmail.com>
2026-03-11 10:08:32 +0100

    Update lcd.c

--- modules/bastardkb/lcd/lcd.c
+++ modules/bastardkb/lcd/lcd.c
@@ -342,17 +342,15 @@ void event_screen_pointer_scroll_toggle(lv_event_t *e) {}
 void event_screen_base_update_mods(lv_event_t *e) {}
 
 void housekeeping_task_lcd(void) {
-    if (is_keyboard_left()) {
-        update_dilemma_status();
+    update_dilemma_status();
 
-        update_layer_name();
-        update_mods();
-        update_rgb_info();
-        update_mouse_info();
-        update_theme_color();
+    update_layer_name();
+    update_mods();
+    update_rgb_info();
+    update_mouse_info();
+    update_theme_color();
 
-        g_dilemma_status_prev = g_dilemma_status;
-    }
+    g_dilemma_status_prev = g_dilemma_status;
 }
 
 void update_layer_name(void) {

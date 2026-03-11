93e6ab68d2e5b12880dedc3890564662b6784675
Quentin <bstkbd@gmail.com>
2026-03-11 09:43:38 +0100

    only init/update screen if keyboard is left

--- modules/bastardkb/lcd/lcd.c
+++ modules/bastardkb/lcd/lcd.c
@@ -110,7 +110,7 @@ ui_theme get_current_theme(void) {
 
 lv_obj_t *ui_create_progress_bar(lv_obj_t *cont, uint8_t flex) {
     lv_obj_t *bar = lv_bar_create(cont);
-    lv_obj_set_height(bar, get_current_theme().bar.height); // TODO make a global variable pointer
+    lv_obj_set_height(bar, get_current_theme().bar.height);
     lv_obj_add_style(bar, &ui_styles.bar, LV_PART_INDICATOR);
     lv_obj_add_style(bar, &ui_styles.bar_background, 0);
     lv_obj_set_flex_grow(bar, flex);
@@ -136,93 +136,93 @@ lv_obj_t *ui_create_line_separator(lv_obj_t *cont, uint8_t flex, uint8_t height)
 }
 
 void keyboard_post_init_lcd(void) {
-    // TODO only do if keyboard is left....
-    // if (is_keyboard_left()) {
-    // Display timeout
-    wait_ms(LCD_WAIT_TIME);
+    if (is_keyboard_left()) {
+        // Display timeout
+        wait_ms(LCD_WAIT_TIME);
 
-    lcd = qp_st7789_make_spi_device(LCD_WIDTH, LCD_HEIGHT, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, LCD_SPI_DIVISOR, SPI_MODE);
-    qp_init(lcd, LCD_ROTATION);
+        lcd = qp_st7789_make_spi_device(LCD_WIDTH, LCD_HEIGHT, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, LCD_SPI_DIVISOR, SPI_MODE);
+        qp_init(lcd, LCD_ROTATION);
 
-    surface = qp_make_rgb565_surface(LCD_WIDTH, LCD_HEIGHT, surface_buffer);
-    qp_init(surface, LCD_ROTATION);
+        surface = qp_make_rgb565_surface(LCD_WIDTH, LCD_HEIGHT, surface_buffer);
+        qp_init(surface, LCD_ROTATION);
 
-    // Display offset
-    qp_set_viewport_offsets(lcd, LCD_OFFSET_X, LCD_OFFSET_Y);
+        // Display offset
+        qp_set_viewport_offsets(lcd, LCD_OFFSET_X, LCD_OFFSET_Y);
 
-    // if(qp_lvgl_attach(lcd)){
-    // TODO is this done automagically? add defines?
-    //    keyboard_post_init_lcd();
-    // }
-    qp_lvgl_attach(lcd);
+        // if(qp_lvgl_attach(lcd)){
+        // TODO is this done automagically? add defines?
+        //    keyboard_post_init_lcd();
+        // }
+        qp_lvgl_attach(lcd);
 
-    // Power on display, fill with black
-    qp_power(lcd, 1);
-    qp_rect(lcd, 0, 0, 300, 300, HSV_BLACK, 1);
-    qp_flush(lcd);
-    ui_screen_base = lv_obj_create(NULL);
+        // Power on display, fill with black
+        qp_power(lcd, 1);
+        qp_rect(lcd, 0, 0, 300, 300, HSV_BLACK, 1);
+        qp_flush(lcd);
+        ui_screen_base = lv_obj_create(NULL);
 
-    init_themes();
-    style_init_all();
+        init_themes();
+        style_init_all();
 
-    lv_obj_t *cont = lv_obj_create(ui_screen_base);
-    lv_obj_set_size(cont, LCD_WIDTH, LCD_HEIGHT);
-    lv_obj_center(cont);
-    lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_ROW_WRAP);
-    lv_obj_add_style(cont, &ui_styles.flex_container, 0);
+        lv_obj_t *cont = lv_obj_create(ui_screen_base);
+        lv_obj_set_size(cont, LCD_WIDTH, LCD_HEIGHT);
+        lv_obj_center(cont);
+        lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_ROW_WRAP);
+        lv_obj_add_style(cont, &ui_styles.flex_container, 0);
 
-    ui_button_layer = lv_btn_create(cont);
-    lv_obj_add_style(ui_button_layer, &ui_styles.layer_name, 0);
-    lv_obj_set_height(ui_button_layer, 33);
-    lv_obj_set_flex_grow(ui_button_layer, 1); // take all remaining space in line
+        ui_button_layer = lv_btn_create(cont);
+        lv_obj_add_style(ui_button_layer, &ui_styles.layer_name, 0);
+        lv_obj_set_height(ui_button_layer, 33);
+        lv_obj_set_flex_grow(ui_button_layer, 1); // take all remaining space in line
 
-    // layer title
-    ui_label_layer = lv_label_create(ui_button_layer);
-    ui_init_layer_name(ui_label_layer);
-    lv_label_set_text(ui_label_layer, "LAYER: BASE");
-    lv_obj_center(ui_label_layer);
+        // layer title
+        ui_label_layer = lv_label_create(ui_button_layer);
+        ui_init_layer_name(ui_label_layer);
+        lv_label_set_text(ui_label_layer, "LAYER: BASE");
+        lv_obj_center(ui_label_layer);
 
-    // mod buttons: SHIFT, ALT, CTRL, GUI
-    mod_buttons[0] = ui_create_mod_button(cont, "SHFT", true, MOD_MASK_SHIFT);
-    mod_buttons[1] = ui_create_mod_button(cont, "ALT", false, MOD_MASK_ALT);
-    mod_buttons[2] = ui_create_mod_button(cont, "CTRL", false, MOD_MASK_CTRL);
-    mod_buttons[3] = ui_create_mod_button(cont, "GUI", false, MOD_MASK_GUI);
+        // mod buttons: SHIFT, ALT, CTRL, GUI
+        mod_buttons[0] = ui_create_mod_button(cont, "SHFT", true, MOD_MASK_SHIFT);
+        mod_buttons[1] = ui_create_mod_button(cont, "ALT", false, MOD_MASK_ALT);
+        mod_buttons[2] = ui_create_mod_button(cont, "CTRL", false, MOD_MASK_CTRL);
+        mod_buttons[3] = ui_create_mod_button(cont, "GUI", false, MOD_MASK_GUI);
 
-    // line separator
-    ui_line_1 = ui_create_line_separator(cont, 1, 3);
+        // line separator
+        ui_line_1 = ui_create_line_separator(cont, 1, 3);
 
-    // display base layer screen upon init
-    lv_disp_load_scr(ui_screen_base);
+        // display base layer screen upon init
+        lv_disp_load_scr(ui_screen_base);
 
-    // mouse special buttons
-    mouse_buttons[0] = ui_create_mod_button(cont, "SNIPE", true, 0);
-    mouse_buttons[1] = ui_create_mod_button(cont, "SCROLL", false, 0);
+        // mouse special buttons
+        mouse_buttons[0] = ui_create_mod_button(cont, "SNIPE", true, 0);
+        mouse_buttons[1] = ui_create_mod_button(cont, "SCROLL", false, 0);
 
-    // sniping DPI widgets
-    ui_label_s_dpi        = ui_create_secondary_text(cont, "SNIPE DPI", true, 4);
-    ui_bar_s_dpi          = ui_create_progress_bar(cont, 4);
-    ui_label_s_dpi_number = ui_create_number_label(cont, 2);
+        // sniping DPI widgets
+        ui_label_s_dpi        = ui_create_secondary_text(cont, "SNIPE DPI", true, 4);
+        ui_bar_s_dpi          = ui_create_progress_bar(cont, 4);
+        ui_label_s_dpi_number = ui_create_number_label(cont, 2);
 
-    // regular DPI widgets
-    ui_label_dpi        = ui_create_secondary_text(cont, "DPI", true, 2);
-    ui_bar_dpi          = ui_create_progress_bar(cont, 6);
-    ui_label_dpi_number = ui_create_number_label(cont, 2);
+        // regular DPI widgets
+        ui_label_dpi        = ui_create_secondary_text(cont, "DPI", true, 2);
+        ui_bar_dpi          = ui_create_progress_bar(cont, 6);
+        ui_label_dpi_number = ui_create_number_label(cont, 2);
 
-    // line separator
-    ui_line_2 = ui_create_line_separator(cont, 1, 3);
+        // line separator
+        ui_line_2 = ui_create_line_separator(cont, 1, 3);
 
-    // rgb widgets
-    ui_label_rgb        = ui_create_secondary_text(cont, "RGB", true, 2);
-    ui_bar_rgb          = ui_create_progress_bar(cont, 6);
-    ui_label_rgb_number = ui_create_number_label(cont, 2);
+        // rgb widgets
+        ui_label_rgb        = ui_create_secondary_text(cont, "RGB", true, 2);
+        ui_bar_rgb          = ui_create_progress_bar(cont, 6);
+        ui_label_rgb_number = ui_create_number_label(cont, 2);
 
-    ui_label_rgb_effect = ui_create_secondary_text(cont, "effect...", true, 1);
+        ui_label_rgb_effect = ui_create_secondary_text(cont, "effect...", true, 1);
 
-    // theme and backgrounds
-    lv_disp_t  *dispp = lv_disp_get_default();
-    lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(BK_PALETTE), lv_palette_main(BK_PALETTE), true, LV_FONT_DEFAULT);
-    lv_disp_set_theme(dispp, theme);
-    lv_obj_set_style_bg_color(cont, lv_color_black(), LV_PART_MAIN);
+        // theme and backgrounds
+        lv_disp_t  *dispp = lv_disp_get_default();
+        lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(BK_PALETTE), lv_palette_main(BK_PALETTE), true, LV_FONT_DEFAULT);
+        lv_disp_set_theme(dispp, theme);
+        lv_obj_set_style_bg_color(cont, lv_color_black(), LV_PART_MAIN);
+    }
 }
 
 void update_styles(ui_theme theme) {
@@ -342,15 +342,17 @@ void event_screen_pointer_scroll_toggle(lv_event_t *e) {}
 void event_screen_base_update_mods(lv_event_t *e) {}
 
 void housekeeping_task_lcd(void) {
-    update_dilemma_status();
+    if (is_keyboard_left()) {
+        update_dilemma_status();
 
-    update_layer_name();
-    update_mods();
-    update_rgb_info();
-    update_mouse_info();
-    update_theme_color();
+        update_layer_name();
+        update_mods();
+        update_rgb_info();
+        update_mouse_info();
+        update_theme_color();
 
-    g_dilemma_status_prev = g_dilemma_status;
+        g_dilemma_status_prev = g_dilemma_status;
+    }
 }
 
 void update_layer_name(void) {

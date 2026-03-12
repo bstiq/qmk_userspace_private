c318d5c7a13d5a448e990c25576ce1a5a70ff584
Quentin <bstkbd@gmail.com>
2026-03-12 13:52:58 +0100

    Update lcd.c

--- modules/bastardkb/lcd/lcd.c
+++ modules/bastardkb/lcd/lcd.c
@@ -529,21 +529,13 @@ const char *rgb_matrix_get_effect_name(void) {
 
 // called by primary, executed by secondary
 void mouse_info_sync_handler(uint8_t initiator2target_buffer_size, const void *initiator2target_buffer, uint8_t target2initiator_buffer_size, void *target2initiator_buffer) {
-    // bool needs_theme_update = false;
     if (initiator2target_buffer_size == sizeof(g_dilemma_status)) {
-        const dilemma_status_t received_status = *(const dilemma_status_t *)initiator2target_buffer;
-        // if (g_dilemma_status.current_theme_id != received_status.current_theme_id) {
-        //     needs_theme_update = true;
-        // }
-
         g_dilemma_status_prev = g_dilemma_status;
-        g_dilemma_status      = received_status;
-
-        // if (needs_theme_update) {
-        //     g_dilemma_config_theme_t.current_theme_id = g_dilemma_status.current_theme_id;
-        //     update_styles(get_current_theme());
-        //     write_dilemma_theme_config_to_eeprom(&g_dilemma_config_theme_t);
-        // }
+        g_dilemma_status      = *(const dilemma_status_t *)initiator2target_buffer;
+        if (g_dilemma_status_prev.current_theme_id != g_dilemma_status.current_theme_id) {
+            g_dilemma_config_theme_t.current_theme_id = g_dilemma_status.current_theme_id;
+            update_styles(get_current_theme());
+        }
 
         refresh_lcd_info();
     }

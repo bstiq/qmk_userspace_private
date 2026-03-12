9c6392ea330554cf82d262cecf631e4c2262bb7a
Quentin <bstkbd@gmail.com>
2026-03-12 12:15:49 +0100

    Update lcd.c

--- modules/bastardkb/lcd/lcd.c
+++ modules/bastardkb/lcd/lcd.c
@@ -512,6 +512,7 @@ bool process_record_lcd(uint16_t keycode, keyrecord_t *record) {
 void cycle_theme(void) {
     g_dilemma_status.current_theme_id = (g_dilemma_status.current_theme_id + 1) % (sizeof(themes) / sizeof(ui_theme *));
     update_styles(get_current_theme());
+    g_dilemma_config_theme_t.current_theme_id = get_current_theme();
     write_dilemma_theme_config_to_eeprom(&g_dilemma_config_theme_t);
 }
 
@@ -542,5 +543,10 @@ void mouse_info_sync_handler(uint8_t initiator2target_buffer_size, const void *i
         g_dilemma_status_prev = g_dilemma_status;
         g_dilemma_status      = *(const dilemma_status_t *)initiator2target_buffer;
         refresh_lcd_info();
+        if(g_dilemma_status_prev.current_theme_id != g_dilemma_status.current_theme_id) {
+            update_styles(get_current_theme());
+            g_dilemma_config_theme_t.current_theme_id = g_dilemma_status.current_theme_id;
+            write_dilemma_theme_config_to_eeprom(g_dilemma_config_theme_t);
+        }
     }
 }

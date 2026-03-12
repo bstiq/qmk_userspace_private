ad80b815d3241367c1c1518a59ea7ce17e4ab0ae
Quentin <bstkbd@gmail.com>
2026-03-12 12:23:45 +0100

    Update lcd.c

--- modules/bastardkb/lcd/lcd.c
+++ modules/bastardkb/lcd/lcd.c
@@ -546,7 +546,7 @@ void mouse_info_sync_handler(uint8_t initiator2target_buffer_size, const void *i
         if(g_dilemma_status_prev.current_theme_id != g_dilemma_status.current_theme_id) {
             update_styles(get_current_theme());
             g_dilemma_config_theme_t.current_theme_id = g_dilemma_status.current_theme_id;
-            write_dilemma_theme_config_to_eeprom(g_dilemma_config_theme_t);
+            write_dilemma_theme_config_to_eeprom(&g_dilemma_config_theme_t);
         }
     }
 }

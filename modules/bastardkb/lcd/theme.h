64746118fcbe234d8e15f3beab3c4538b0e2010e
Quentin <bstkbd@gmail.com>
2026-03-20 14:17:26 +0100

    try to add dynamic array of themes, does not work.

--- modules/bastardkb/lcd/theme.h
+++ modules/bastardkb/lcd/theme.h
@@ -15,7 +15,7 @@ typedef union {
     } __attribute__((packed));
 } dilemma_status_theme_t;
 
-dilemma_status_theme_t dilemma_lcd_theme;
+dilemma_status_theme_t dilemma_theme_config;
 
 // TODO once removed ui_styles from lcd.c, move this into ui_elements.c to make it private to ui_elements 
 typedef struct {
@@ -31,7 +31,17 @@ typedef struct {
     lv_style_t value_labels;
 } ui_styles_t;
 
-ui_styles_t ui_styles;
+typedef struct{
+    ui_styles_t **array;
+    size_t used;
+    size_t size;
+    int amount_elements;
+} obj_theme_array_t;
+
+obj_theme_array_t obj_theme_array;
+
+void add_obj_theme_array(obj_theme_array_t *a, ui_styles_t *element);
+void init_obj_theme_array(obj_theme_array_t *a);
 
 void init_themes(void);
 void read_dilemma_theme_config_from_eeprom(dilemma_status_theme_t *config);
@@ -42,3 +52,4 @@ void     init_styles(void);
 void     update_styles_from_current_theme(void);
 uint8_t  get_current_theme_id(void);
 void set_current_theme_id(uint8_t id);
+ui_styles_t *get_current_theme(void);
--- modules/bastardkb/lcd/ui_elements.c
+++ modules/bastardkb/lcd/ui_elements.c
@@ -5,24 +5,27 @@
 
 lv_obj_t *ui_create_progress_bar(lv_obj_t *cont, uint8_t flex) {
     lv_obj_t *bar = lv_bar_create(cont);
+    ui_styles_t *ui_styles = get_current_theme();
     // lv_obj_set_height(bar, get_current_theme().bar.height);
-    lv_obj_add_style(bar, &ui_styles.bar, LV_PART_INDICATOR);
-    lv_obj_add_style(bar, &ui_styles.bar_background, 0);
+    lv_obj_add_style(bar, &(ui_styles->bar), LV_PART_INDICATOR);
+    lv_obj_add_style(bar, &(ui_styles->bar_background), 0);
     lv_obj_set_flex_grow(bar, flex);
     return bar;
 }
 
 lv_obj_t *ui_create_number_label(lv_obj_t *cont, uint8_t flex) {
     lv_obj_t *lbl = lv_label_create(cont);
+    ui_styles_t *ui_styles = get_current_theme();
     lv_label_set_text(lbl, "1234");
-    lv_obj_add_style(lbl, &ui_styles.value_labels, 0);
+    lv_obj_add_style(lbl, &(ui_styles->value_labels), 0);
     lv_obj_set_flex_grow(lbl, flex);
     return lbl;
 }
 
 lv_obj_t *ui_create_layer_label(lv_obj_t *cont){
     lv_obj_t *ui_button_layer = lv_btn_create(cont);
-    lv_obj_add_style(ui_button_layer, &ui_styles.layer_name, 0);
+    ui_styles_t *ui_styles = get_current_theme();
+    lv_obj_add_style(ui_button_layer, &(ui_styles->layer_name), 0);
     lv_obj_set_height(ui_button_layer, 33);
     lv_obj_set_flex_grow(ui_button_layer, 1); // take all remaining space in line
 
@@ -39,18 +42,18 @@ lv_obj_t *ui_create_layer_label(lv_obj_t *cont){
 
 lv_obj_t *ui_create_line_separator(lv_obj_t *cont, uint8_t flex, uint8_t height) {
     lv_obj_t *bar = lv_bar_create(cont);
+    ui_styles_t *ui_styles = get_current_theme();
     lv_obj_add_flag(bar, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
     lv_obj_set_flex_grow(bar, flex);
     lv_obj_set_height(bar, height);
-    lv_obj_add_style(bar, &ui_styles.line, LV_PART_INDICATOR);
-    lv_obj_add_style(bar, &ui_styles.line_background, 0);
+    lv_obj_add_style(bar, &(ui_styles->line), LV_PART_INDICATOR);
+    lv_obj_add_style(bar, &(ui_styles->line_background), 0);
     return bar;
 }
 
 // todo get rid of mod_button_pair_t structure
 lv_obj_t *ui_create_mod_button(lv_obj_t *cont, const char *text, bool force_new_track, uint8_t mod_mask) {
     mod_button_pair_t b = {0};
-
     b.mod_mask = mod_mask;
     b.button   = lv_btn_create(cont);
     ui_init_button_mod_indicator(b.button);
@@ -66,16 +69,17 @@ lv_obj_t *ui_create_mod_button(lv_obj_t *cont, const char *text, bool force_new_
 }
 
 void ui_init_button_mod_indicator(lv_obj_t *button) {
-    lv_obj_add_style(button, &ui_styles.mod_btn, 0);
-    lv_obj_add_style(button, &ui_styles.mod_btn_pressed, LV_STATE_PRESSED);
-    lv_obj_set_height(button, 33);
+    ui_styles_t *ui_styles = get_current_theme();
+    lv_obj_add_style(button, &(ui_styles->mod_btn), 0);
+    lv_obj_add_style(button, &(ui_styles->mod_btn_pressed), LV_STATE_PRESSED);
     lv_obj_set_flex_grow(button, 1);
 }
 
 lv_obj_t *ui_create_secondary_text(lv_obj_t *cont, const char *text, bool new_track, uint8_t flex) {
     lv_obj_t *lbl = lv_label_create(cont);
+    ui_styles_t *ui_styles = get_current_theme();
     lv_label_set_text(lbl, text);
-    lv_obj_add_style(lbl, &ui_styles.secondary_labels, 0);
+    lv_obj_add_style(lbl, &(ui_styles->secondary_labels), 0);
     if (new_track) {
         lv_obj_add_flag(lbl, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
     }

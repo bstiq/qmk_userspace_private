6b6b7d86131ca84cce0c735d7ba493c78f0926d3
Quentin <bstkbd@gmail.com>
2026-04-06 17:05:56 +0200

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/bwpo/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/bwpo/keymap.c
@@ -36,6 +36,7 @@ enum dilemma_keymap_layers {
 #define ESC_MED LT(LAYER_MEDIA, KC_ESC)
 #define SPC_NAV LT(LAYER_NAVIGATION, KC_SPC)
 #define TAB_FUN LT(LAYER_FUNCTION, KC_TAB)
+#define TAB_NAV LT(LAYER_NAVIGATION, KC_TAB)
 #define ENT_SYM LT(LAYER_SYMBOLS, KC_ENT)
 #define BSP_NUM LT(LAYER_NUMERAL, KC_BSPC)
 #define PT_Z LT(LAYER_POINTER, KC_Z)
@@ -63,7 +64,7 @@ enum dilemma_keymap_layers {
        KC_B,    KC_W,    KC_P,    KC_O,    KC_QUOT,    KC_DOT,    KC_V,    KC_D,    KC_L,    KC_J, \
        KC_A,    KC_U,    KC_I,    KC_E,    KC_COMM,    KC_C,    KC_T,    KC_S,    KC_R, KC_N, \
        KC_Z,    KC_Y,    KC_X,    KC_SLSH,    KC_K,    KC_M,    KC_Q, KC_G,  KC_H, KC_F, \
-                      ESC_MED, TAB_FUN, TAB_FUN, SPC_NUM, ENT_SYM, MO(LAYER_LCD)
+                      ESC_MED, ESC_MED, TAB_NAV, SPC_NUM, ENT_SYM, MO(LAYER_LCD)
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -96,7 +97,7 @@ enum dilemma_keymap_layers {
 
   /**
    * \brief Media layer.
-   *
+   *   
    * Tertiary left- and right-hand layer is media and RGB control.  This layer is
    * symmetrical to accomodate the left- and right-hand trackball.
    */

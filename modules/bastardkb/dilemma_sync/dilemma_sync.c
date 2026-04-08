164e34926ed49dfbfe4e35df1888dfe4a9ac5deb
Quentin <bstkbd@gmail.com>
2026-04-08 13:36:39 +0200

    sync only if needed

--- modules/bastardkb/dilemma_sync/dilemma_sync.c
+++ modules/bastardkb/dilemma_sync/dilemma_sync.c
@@ -3,22 +3,23 @@
 #include "transactions.h"
 
 dilemma_status_t dilemma_status = { 0 };
+dilemma_status_t dilemma_status_prev = { 0 };
 
 void update_dilemma_status(void);
 void dilemma_sync_handler(uint8_t initiator2target_buffer_size, const void* initiator2target_buffer, uint8_t target2initiator_buffer_size, void* target2initiator_buffer);
 
 void keyboard_post_init_dilemma_sync(void) {
-    // register rpc handlers for syncing
     transaction_register_rpc(RPC_ID_DILEMMA_SYNC, dilemma_sync_handler);
 }
 
 void housekeeping_task_dilemma_sync(void) {
-    // we only want to sync the dilemma status from the right to the left side, since the left side is where the screen is, and where the status is used.
     if (is_keyboard_master()) {
         update_dilemma_status();
         // TODO retry sync if it fails
-        // TODO only send it if it's different from the last sent status? or, only send the parts that are different? or, send it every x seconds if it's different, to make sure it gets synced even if a transaction fails?
-        transaction_rpc_send(RPC_ID_DILEMMA_SYNC, sizeof(dilemma_status), &dilemma_status);
+        if(memcmp(&dilemma_status, &dilemma_status_prev, sizeof(dilemma_status_t))) {
+            transaction_rpc_send(RPC_ID_DILEMMA_SYNC, sizeof(dilemma_status), &dilemma_status);
+            dilemma_status_prev = dilemma_status;
+        }
     }
 }
 

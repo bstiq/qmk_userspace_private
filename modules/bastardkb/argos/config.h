daf981c12123d6dd99fd547ab058c934f816a6aa
bstkbd@gmail.com <bstkbd@gmail.com>
2026-05-08 09:11:56 +0200

    switch to via compatible protocol for now

--- modules/bastardkb/argos/config.h
+++ modules/bastardkb/argos/config.h
@@ -3,8 +3,8 @@
 
 #pragma once
 
-#define RAW_USAGE_PAGE 0xFF61
-#define RAW_USAGE_ID 0x62
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
 
 #ifdef EXTRA_EXTRA_LONG_COMBOS
 #undef EXTRA_EXTRA_LONG_COMBOS

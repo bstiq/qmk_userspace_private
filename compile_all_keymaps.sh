46e599f59353ffcc51226607a3e74a873aca6c97
Quentin <bstkbd@gmail.com>
2026-05-14 11:47:23 +0200

    add dilemma35 and dilemma46

--- compile_all_keymaps.sh
+++ compile_all_keymaps.sh
@@ -9,5 +9,9 @@ git switch bkb-develop
 cd ../qmk_userspace_private
 qmk compile -c -kb bastardkb/charybdis/4x6/splinktegrated_rev1 -km vendor
 qmk compile -c -kb bastardkb/charybdis/3x5/splinktegrated_rev1 -km vendor
+qmk compile -c -kb bastardkb/charybdis/3x6/splinktegrated_rev1 -km vendor
 qmk compile -c -kb bastardkb/scylla/splinktegrated_rev1 -km vendor
 qmk compile -c -kb bastardkb/skeletyl/splinktegrated_rev1 -km vendor
+qmk compile -c -kb bastardkb/tbkmini/splinktegrated_rev1 -km vendor
+qmk compile -c -kb bastardkb/dilemma/3x5_3 -km vendor
+qmk compile -c -kb bastardkb/dilemma/4x6_4 -km vendor

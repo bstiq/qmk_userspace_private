aa72b2ad50158a3cb9965cfba7a86b786107339a
Quentin <bstkbd@gmail.com>
2026-06-16 15:57:58 +0200

    rgb work

--- modules/bastardkb/argos/argos_rgb.c
+++ modules/bastardkb/argos/argos_rgb.c
@@ -12,8 +12,9 @@ void argos_rgb_init(void) {
     // default: per-layer rgb
     // TODO remove hardcoded 10 layers max value
     for(int layer = 1; layer < 10; layer++) {
+        const uint8_t brightness = rgb_matrix_get_val();
         // pick 10 different colors, easier to do in HSV
-        HSV hsv = (HSV){layer * 360 / 10, 255, 255};
+        HSV hsv = (HSV){layer * 360 / 10, 255, brightness};
         RGB rgb = hsv_to_rgb(hsv);
         for(int i = 0; i < RGB_ENTRIES_PER_LAYER; i++) {
             argos_rgb_entries[layer * RGB_ENTRIES_PER_LAYER + i] = (argos_rgb_t){rgb.r, rgb.g, rgb.b, false, true, true};
@@ -66,15 +67,8 @@ bool rgb_matrix_indicators_advanced_argos(uint8_t led_min, uint8_t led_max) {
         if(argos_rgb_entries[index].custom) {
             if(argos_rgb_entries[index].on) {
                 if(argos_rgb_entries[index].passthrough == false) {
-                    // manually convert RGB to HSV
-                    hsv_t hsv = rgb_to_hsv(argos_rgb_entries[index].r, argos_rgb_entries[index].g, argos_rgb_entries[index].b);
-
-                    // set to current brightness
-                    hsv.v = rgb_matrix_get_val();
-
-                    // convert back to rgb
-                    rgb_t rgb = hsv_to_rgb(hsv);
-                    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+                    // rgb_t rgb = key_with_max_brightness(argos_rgb_entries[index].r, argos_rgb_entries[index].g, argos_rgb_entries[index].b, reference_brightness);
+                    rgb_matrix_set_color(i, argos_rgb_entries[index].r, argos_rgb_entries[index].g, argos_rgb_entries[index].b);
                 }
             } else {
                 rgb_matrix_set_color(i, 0, 0, 0);
@@ -142,52 +136,59 @@ void argos_rgb_get_led_at_position(argos_rgb_t *entry, uint8_t layer, uint8_t in
     *entry = argos_rgb_entries[baseIndex + index + offset];
 }
 
-hsv_t rgb_to_hsv(uint8_t red, uint8_t green, uint8_t blue)
-{
-    double h, s, v;
-    double      min, max, delta;
+// rgb_t key_with_max_brightness(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness) {
+//     hsv_t hsv = rgb_to_hsv(r, g, b);
+//     hsv.v = brightness;
+//     return hsv_to_rgb(hsv);
+// }
 
-    min = red < green ? red : green;
-    min = min  < blue ? min  : blue;
+// // Here we don't care about V, since we will set it to the global brightness later
+// hsv_t rgb_to_hsv(uint8_t red, uint8_t green, uint8_t blue)
+// {
+//     double h, s, v;
+//     double      min, max, delta;
 
-    max = red > green ? red : green;
-    max = max  > blue ? max  : blue;
+//     min = red < green ? red : green;
+//     min = min  < blue ? min  : blue;
 
-    v = max;                                // v
-    delta = max - min;
-    if (delta < 0.00001)
-    {
-        s = 0;
-        h = 0; // undefined, maybe nan?
-        return (hsv_t){h, s, v};
-    }
-    if( max > 0.0 ) { // NOTE: if Max is == 0, this divide would cause a crash
-        s = (delta / max);                  // s
-    } else {
-        // if max is 0, then r = g = b = 0              
-        // s = 0, h is undefined
-        s = 0;
-        h = 0;                            // its now undefined
-        return (hsv_t){h, s, v};
-    }
-    if( red >= max )                           // > is bogus, just keeps compilor happy
-        h = ( green - blue ) / delta;        // between yellow & magenta
-    else if( green >= max )
-        h = 2.0 + ( blue - red ) / delta;  // between cyan & yellow
-    else
-        h = 4.0 + ( red - green ) / delta;  // between magenta & cyan
+//     max = red > green ? red : green;
+//     max = max  > blue ? max  : blue;
 
-    h *= 60.0;                              // degrees
+//     v = max;                                // v
+//     delta = max - min;
+//     if (delta < 0.00001)
+//     {
+//         s = 0;
+//         h = 0; // undefined, maybe nan?
+//         return (hsv_t){h, s, v};
+//     }
+//     if( max > 0.0 ) { // NOTE: if Max is == 0, this divide would cause a crash
+//         s = (delta / max);                  // s
+//     } else {
+//         // if max is 0, then r = g = b = 0              
+//         // s = 0, h is undefined
+//         s = 0;
+//         h = 0;                            // its now undefined
+//         return (hsv_t){h, s, v};
+//     }
+//     if( red >= max )                           // > is bogus, just keeps compilor happy
+//         h = ( green - blue ) / delta;        // between yellow & magenta
+//     else if( green >= max )
+//         h = 2.0 + ( blue - red ) / delta;  // between cyan & yellow
+//     else
+//         h = 4.0 + ( red - green ) / delta;  // between magenta & cyan
 
-    if( h < 0.0 )
-        h += 360.0;
+//     h *= 60.0;                              // degrees
 
-    // align with QMK (expects values between 0 and 255)
-    hsv_t out = (hsv_t){0, 0, 0};
-    out.s = (uint8_t)(s * 255); // align with QMK
-    out.v = (uint8_t)(v * 255); // align with QMK
-    out.h = (uint8_t)(h/360 * 255); // align with QMK
+//     if( h < 0.0 )
+//         h += 360.0;
 
-    return out;
-}
+//     // align with QMK (expects values between 0 and 255)
+//     hsv_t out = (hsv_t){0, 0, 0};
+//     out.s = (uint8_t)(s * 255); // align with QMK
+//     out.v = (uint8_t)(v * 255); // align with QMK
+//     out.h = (uint8_t)(h/360 * 255); // align with QMK
+
+//     return out;
+// }
 

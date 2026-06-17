2ebdd041a7e92cbeb1abae2c199910f03b87a4a2
Quentin <bstkbd@gmail.com>
2026-06-17 15:42:12 +0200

    rgb work

--- modules/bastardkb/argos/argos_rgb.c
+++ modules/bastardkb/argos/argos_rgb.c
@@ -67,8 +67,20 @@ bool rgb_matrix_indicators_advanced_argos(uint8_t led_min, uint8_t led_max) {
         if(argos_rgb_entries[index].custom) {
             if(argos_rgb_entries[index].on) {
                 if(argos_rgb_entries[index].passthrough == false) {
-                    // rgb_t rgb = key_with_max_brightness(argos_rgb_entries[index].r, argos_rgb_entries[index].g, argos_rgb_entries[index].b, reference_brightness);
-                    rgb_matrix_set_color(i, argos_rgb_entries[index].r, argos_rgb_entries[index].g, argos_rgb_entries[index].b);
+
+                    // TODO: store the V value in the argos_rgb_t struct
+                    // then we can compare it against the current brightness
+                    // and if needed, we do the whole conversion to HSV and back.
+
+                    // manually convert RGB to HSV
+                    hsv_t hsv = rgb_to_hsv(argos_rgb_entries[index].r, argos_rgb_entries[index].g, argos_rgb_entries[index].b);
+
+                    // set to current brightness
+                    hsv.v = rgb_matrix_get_val();
+
+                    // convert back to rgb
+                    rgb_t rgb = hsv_to_rgb(hsv);
+                    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
                 }
             } else {
                 rgb_matrix_set_color(i, 0, 0, 0);
@@ -127,4 +139,60 @@ void rgb_sync_handler(uint8_t initiator2target_buffer_size, const void* initiato
 void argos_rgb_get_led_at_position(argos_rgb_t *entry, uint8_t layer, uint8_t index, uint8_t offset) {
     uint16_t baseIndex = layer * RGB_ENTRIES_PER_LAYER;
     *entry = argos_rgb_entries[baseIndex + index + offset];
+}
+
+rgb_t key_with_max_brightness(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness) {
+    hsv_t hsv = rgb_to_hsv(r, g, b);
+    hsv.v = brightness;
+    return hsv_to_rgb(hsv);
+}
+
+// Here we don't care about V, since we will set it to the global brightness later
+hsv_t rgb_to_hsv(uint8_t red, uint8_t green, uint8_t blue)
+{
+    double h, s, v;
+    double      min, max, delta;
+
+    min = red < green ? red : green;
+    min = min  < blue ? min  : blue;
+
+    max = red > green ? red : green;
+    max = max  > blue ? max  : blue;
+
+    v = max;                                // v
+    delta = max - min;
+    if (delta < 0.00001)
+    {
+        s = 0;
+        h = 0; // undefined, maybe nan?
+        return (hsv_t){h, s, v};
+    }
+    if( max > 0.0 ) { // NOTE: if Max is == 0, this divide would cause a crash
+        s = (delta / max);                  // s
+    } else {
+        // if max is 0, then r = g = b = 0              
+        // s = 0, h is undefined
+        s = 0;
+        h = 0;                            // its now undefined
+        return (hsv_t){h, s, v};
+    }
+    if( red >= max )                           // > is bogus, just keeps compilor happy
+        h = ( green - blue ) / delta;        // between yellow & magenta
+    else if( green >= max )
+        h = 2.0 + ( blue - red ) / delta;  // between cyan & yellow
+    else
+        h = 4.0 + ( red - green ) / delta;  // between magenta & cyan
+
+    h *= 60.0;                              // degrees
+
+    if( h < 0.0 )
+        h += 360.0;
+
+    // align with QMK (expects values between 0 and 255)
+    hsv_t out = (hsv_t){0, 0, 0};
+    out.s = (uint8_t)(s * 255); // align with QMK
+    out.v = (uint8_t)(v * 255); // align with QMK
+    out.h = (uint8_t)(h/360 * 255); // align with QMK
+
+    return out;
 }
--- modules/bastardkb/argos/argos_rgb.h
+++ modules/bastardkb/argos/argos_rgb.h
@@ -8,6 +8,8 @@ void argos_rgb_set_led_at_position(uint8_t layer, uint8_t row, uint8_t col, uint
 void argos_rgb_get_led_at_position(argos_rgb_t *entry, uint8_t layer, uint8_t index, uint8_t offset);
 void rgb_sync_handler(uint8_t initiator2target_buffer_size, const void* initiator2target_buffer, uint8_t target2initiator_buffer_size, void* target2initiator_buffer);
 void argos_rgb_handle_set_led_at_position(uint16_t keyIndex, uint8_t r, uint8_t g, uint8_t b, bool passthrough, bool on, bool custom);
+rgb_t key_with_max_brightness(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness);
+hsv_t rgb_to_hsv(uint8_t red, uint8_t green, uint8_t blue);
 
 // for override purposes
 // bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max);

c97067adbd0d088ee75597707b5d28459aeee2c0
Quentin <bstkbd@gmail.com>
2026-07-07 14:54:51 +0200

    disable encoders on dilemma/3x5_3_trackball

--- compile_all_keymaps.sh
+++ compile_all_keymaps.sh
@@ -5,6 +5,8 @@ qmk git-submodule
 cd ../qmk_userspace_private
 qmk compile -c -kb bastardkb/dilemma/3x5_3_procyon -km bwpo
 qmk compile -c -kb bastardkb/dilemma/3x5_3_procyon -km vendor
+qmk compile -c -kb bastardkb/dilemma/3x5_3_trackball -km bwpo
+qmk compile -c -kb bastardkb/dilemma/3x5_3_trackball -km vendor
 qmk compile -c -kb bastardkb/dilemma/4x6_4_procyon -km vendor
 qmk compile -c -kb bastardkb/charybdis/4x6/splinktegrated_rev1 -km vendor
 qmk compile -c -kb bastardkb/charybdis/3x5/splinktegrated_rev1 -km vendor

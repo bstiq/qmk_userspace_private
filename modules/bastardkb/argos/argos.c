6b78ba1196da4de1dfbf45a703c6c3b5d4438404
ddyo <danrosenshain@gmail.com>
2026-07-23 01:45:08 +0300

    fix: allow external VIA command dispatch

--- modules/bastardkb/argos/argos.c
+++ modules/bastardkb/argos/argos.c
@@ -443,16 +443,12 @@ bool argos_handle_command(uint8_t *data, uint8_t length) {
 }
 
 // Override via_command_kb to intercept Via protocol commands
+#ifndef ARGOS_DISABLE_VIA_COMMAND_KB
 bool via_command_kb(uint8_t *data, uint8_t length) {
     // try to handle it with argos
-    bool result = argos_handle_command(data, length);
-    if (result) {
-        return true;
-    } else {
-        return false;
-    }
-    return false;
+    return argos_handle_command(data, length);
 }
+#endif // ARGOS_DISABLE_VIA_COMMAND_KB
 
 bool process_record_argos(uint16_t keycode, keyrecord_t *record) {
     // are we capturing all keycodes? (testing keymap)

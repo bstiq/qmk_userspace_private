b8b5e357258012e034cacd74f65f173d464542d3
Quentin <bstkbd@gmail.com>
2026-07-23 13:16:09 +0200

    Update compile_all_keymaps.sh

--- compile_all_keymaps.sh
+++ compile_all_keymaps.sh
@@ -3,10 +3,7 @@ cd ../bastardkb-qmk
 git switch bkb-pointing-device
 qmk git-submodule
 cd ../qmk_userspace_private
-qmk compile -c -kb bastardkb/dilemma/3x5_3_procyon -km bwpo
 qmk compile -c -kb bastardkb/dilemma/3x5_3_procyon -km vendor
-qmk compile -c -kb bastardkb/dilemma/3x5_3_trackball -km bwpo
-qmk compile -c -kb bastardkb/dilemma/3x5_3_trackball -km vendor
 qmk compile -c -kb bastardkb/dilemma/4x6_4_procyon -km vendor
 qmk compile -c -kb bastardkb/charybdis/4x6/splinktegrated_rev1 -km vendor
 qmk compile -c -kb bastardkb/charybdis/3x5/splinktegrated_rev1 -km vendor

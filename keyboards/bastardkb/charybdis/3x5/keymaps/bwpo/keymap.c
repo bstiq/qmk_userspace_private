704cc7cb0eead074c9776cb02c19c311c0bb4ef1
Quentin <bstkbd@gmail.com>
2026-07-30 16:17:32 +0200

    update my keymap

--- keyboards/bastardkb/charybdis/3x5/keymaps/bwpo/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/bwpo/keymap.c
@@ -70,10 +70,10 @@ static uint16_t auto_pointer_layer_timer = 0;
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-       KC_B,    KC_W,    KC_P,    KC_O,    KC_QUOT,    MA_TOGG,    KC_V,    KC_D,    KC_L,    KC_J, \
-       KC_A,    KC_U,    KC_I,    KC_E,    KC_COMM,    MA_TKOF,    MA_GROW,    MA_OFST,    MA_LMT, KC_N, \
+       KC_B,    KC_W,    KC_P,    KC_O,    KC_QUOT,    KC_DOT,    KC_V,    KC_D,    KC_L,    KC_J, \
+       KC_A,    KC_U,    KC_I,    KC_E,    KC_COMM,    KC_C,    KC_T,    KC_S,    KC_R, KC_N, \
        KC_Z,    KC_Y,    KC_X,    KC_SLSH,    KC_K,    KC_M,    KC_Q, KC_G,  KC_H, KC_F, \
-                      ESC_MED, SPC_NAV, TAB_FUN, ENT_SYM, SPC_NUM
+                      ESC_MED, TAB_FUN, TAB_FUN, ENT_SYM, SPC_NUM
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
